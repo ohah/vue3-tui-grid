d914ff8cd9112436a1fe643734f4a0c6538e24e9
ohah <bookyoon173@gmail.com>
2022-07-20 02:04:29 +0900

    resolve fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from "url";
-
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import typescript2 from "rollup-plugin-typescript2";
 import vue from "@vitejs/plugin-vue";
@@ -25,7 +24,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@/": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": resolve(__dirname, "src"),
     },
   },
   build: {
